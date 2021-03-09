efb566902e23cadeb5dbcb2b5aed866305c8e7cd
marleigh <volcana@gmail.com>
2021-03-08 16:00:39 -0800

    adding in more beer results

--- script.js
+++ script.js
@@ -5,20 +5,40 @@ let url ='https://api.punkapi.com/v2/beers/random';
 
 btn.addEventListener('click', fetchMe);
 
+//will need multiple listeners on the various buttons chosen
+    //ABV
+    //IBU
+
 async function fetchMe() {
 
     let fetchBeer = await fetch(url);
     let json = await fetchBeer.json();
 
     console.log(json);
-    let beerName = json[0].name;
-    let beerABV = json[0].abv;
-    let beerIBU = json[0].ibu;
-    let beerTagline = json[0].tagline;
-    // need to adjust for when multiple beers get pulled in
-    
-    let cardDiv = document.createElement('div');
-    cardDiv.textContent = `Your beer is: ${beerName}`;
-    cardDiv.classList.add('result-card');
-    resultCard.appendChild(cardDiv);
+
+    json.forEach(beer => {
+
+        let cardDiv = createDiv('result-card');
+        let infoCard = createDiv('info-card');
+        let imgDiv = createDiv('img-div');
+        let img = document.createElement('img');
+        img.src = beer.image_url;
+        img.classList.add('img-div');
+        imgDiv.appendChild(img);
+
+
+        cardDiv.textContent = `Your beer is: ${beer.name}, ${beer.tagline}`;
+        infoCard.textContent = `ABV: ${beer.abv} IBU: ${beer.ibu}`;
+        
+        resultCard.appendChild(imgDiv);
+        resultCard.appendChild(cardDiv);
+        cardDiv.appendChild(infoCard);
+    });
+
+    }
+
+    function createDiv(className) {
+        let div = document.createElement('div');
+        div.classList.add(className);
+        return div;
     }
